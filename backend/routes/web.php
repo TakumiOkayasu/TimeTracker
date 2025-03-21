6563f5c1fb03827187fc1c798c1fa0c0a157eb8e
Takumi Okayasu <takumi_okys@icloud.com>
2025-03-21 16:59:00 +0900

    fix: change how to write

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'Welcome to API'], 200);
-});
+Route::get('/', fn() => response()->json(['message' => 'Welcome to API'], 200));
